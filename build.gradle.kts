9a6954e45761b8206c6c68c8e135d3eabf751c6c
Lyndsey Winter <lynwin@pm.me>
2022-04-03 00:41:21 -0300

    🔖 Bump version to 2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ subprojects {
             create<MavenPublication>(project.name) {
                 groupId = "live.mcparty"
                 artifactId = "netherboard-${project.name}"
-                version = "2.0.0"
+                version = "2.1.0"
 
                 from(components["java"])
             }
